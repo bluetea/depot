6032afb527a8bfd4efe782c3bf63e99e468f4e65
tea <bluetea@gmail.com>
2014-01-20 11:04:06 +0800

    dd

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "product attributes must be not be empty" do
+  	product = Product.new
+
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+
+  test "poduct price must be positive" do
+  	product = Product.new(title: "my book title",
+  		description: "my book description",
+  		image_url: "aaa.jpg"
+  		)
+
+  	# product.price = -1
+  	# assert product.invalid?
+  	# assert_equal '必须大于0.01', product.errors[:price]
+
+  	# product.price = 0
+  	# assert product.invalid?
+  	# assert_equal '必须大于0.01', product.errors[:price]
+
+  	product.price = 1
+  	assert product.valid?
+  end
+
+
+
 end
